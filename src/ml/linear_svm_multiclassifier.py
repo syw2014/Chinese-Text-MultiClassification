3c186f75e93d9c2f8a7af9769d8949287df74701
jerryshi <jerryshi0110@hotmail.com>
2017-04-17 16:05:36 +0800

    added a new folder for machine learning algorithms, which will be a complete system for a specific application

---
+++ src/ml/linear_svm_multiclassifier.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#!/usr/bin/env python
+
+# Author : jerry.Shi
+# Description:
+#      A multi-class classification  based on linear svm, it can be uesed in the produce environment.
+
+# step 0, load filter words for feature generation 
+
+# step 1, generate feature words
+# def gen_feature_words(filename, topk=10000)
+# step 2, vectorize training sample
+# step 3, training classifier
+# step 4, predict with probability
+
