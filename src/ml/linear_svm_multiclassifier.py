39e24fba8507b02da3ad7f77a98b7a1a609b76f5
jerryshi <jerryshi0110@hotmail.com>
2017-04-19 18:41:40 +0800

    finished design feature selection for training corpus, firstly select feature words based on tf in each label, then select global features based on idf

--- src/ml/linear_svm_multiclassifier.py
+++ src/ml/linear_svm_multiclassifier.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[13]:
 
 #!/usr/bin/env python
 
@@ -9,7 +9,10 @@
 # Description:
 #      A multi-class classification  based on linear svm, it can be uesed in the produce environment.
 
+import sys
 import numpy as np
+import math
+from __future__ import division
 
 # step 0, load filter words for feature generation 
 def load_stopwords(filename):
@@ -57,10 +60,77 @@ def gen_feature_words(data, topk=10000):
     print("All training samples loaded, total sample number: %d, total label number: %d") %(total_sample_size, len(label_samples))
     
     # TODO: data unbalance process
-    label_sample_num = []
-    for x in label_samples.values():
-        label_samples.append(x)
-    max_label_num = np.array(label_sample_num).max()
+    label_sample_num = {}
+    for k, v in label_samples.items():
+        label_sample_num[k] = len(v)
+    
+    # get maximum samples number of label
+    max_label_num = np.array(label_sample_num.values()).max()
+    
+    # compute label sample weight based on training samples number
+    label_sample_weight = label_sample_num
+    for k, v in label_sample_num.items():
+        label_sample_weight[k] = (max_label_num) / (v + 1.0)
+
+    # select feature words for every label based on term frequency
+    k_words = 100
+    vocab = {} # key: label id, val : feature words
+    for label, samples in label_samples.items():
+        token_tf = {}
+        for txt in samples:
+            txt = txt.strip()
+            if len(txt) == 0:
+                continue
+            cxt = txt.split(" ")
+            # add token to token_tf
+            for tok in cxt:
+                if len(tok) < 1:
+                    continue
+                if token_tf.has_key(tok):
+                    token_tf[tok] += 1  # tf + 1
+                else:
+                    token_tf[tok] = 1
+        # sort by tf 
+        token_tf = dict(sorted(token_tf.iteritems(), key = lambda d:d[1], reverse=True))
+        # select k words as feature 
+        feature_words = token_tf.keys()[: k_words]
+        
+        # insert label feature words into global vocabulary
+        vocab[label] = feature_words
+    
+    # select global feature based on inverse label frequency
+    global_features = {}  # key: term, value: idf
+    words = vocab.values()
+    label_size = len(vocab) # label number
+    for label, tokens in vocab.items():
+
+        for tok in tokens:
+            df = 0
+            i = 0
+            for i in range(len(words)):
+                if tok in words[i]:
+                    df += 1
+            # add word to global feature dict
+            if global_features.has_key(tok):
+                continue
+            else:
+                idf = math.log(label_size / (df + 1.0))
+                global_features[tok] = idf
+    
+    # select topk term as global feature
+    featue_size = 20000
+    global_features = dict(sorted(global_features.iteritems(), key= lambda d:d[1], reverse=True))
+    features = global_features.keys()[:featue_size]
+    weights = global_features.values()[:featue_size]
+    print("global feature generation completed, total size: %d") % (len(features))
+    # final global features
+    tmp = dict(zip(features, weights))
+    with open('test.txt', 'w') as ofs:
+        for k, v in tmp.items():
+            ofs.write(k + "\t" + str(v))
+            ofs.write('\n')
+            
+    return dict(zip(features, weights))
     
     
             
@@ -73,6 +143,9 @@ def gen_feature_words(data, topk=10000):
 if __name__ == "__main__":
     stopwords_file = "stopwords.txt"
     stop_words = load_stopwords(stopwords_file)
+    
+    train_sample = "train_test.txt"
+    gen_feature_words(train_sample, 20000)
 
 
 # In[ ]:
